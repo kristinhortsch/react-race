9d22f7759078c9cfd63722209689520f535946da
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-18 17:23:32 -0700

    passing test

---
+++ test/random.test.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+require('../lib/utils/connect')();
+const request = require('supertest');
+const app = require('../lib/app');
+const mongoose = require('mongoose');
+const seedData = require('./seedData');
+
+describe('chirp model', () => {
+  beforeEach(() => {
+    return seedData();
+  });
+
+  afterEach(done => {
+    mongoose.connection.dropDatabase(done);
+    
+  });
+
+  afterAll(done => {
+    mongoose.connection.close(done);
+  });
+
+  it('gets a list of all chirps', () => {
+    return request(app)
+      .get('/random')
+      .then(res => {
+        expect(res.body).toHaveLength(1);
+      });
+  });
+});
+
